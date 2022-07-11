f718e6de180bee24603bef923b39e65d57deec0c
Mizan <mizan@Mizans-Mac-mini.local>
2022-07-11 13:53:44 +0600

    feed - [create post]

--- client/src/components/Feed/Feed.component.tsx
+++ client/src/components/Feed/Feed.component.tsx
@@ -1,7 +1,14 @@
 import React from "react";
+import Share from "../share/Share.component";
 
 type Props = {};
 
 export default function Feed({}: Props) {
-  return <div className="flex-[5.5]">Freed.component</div>;
+  return (
+    <div className="flex-[5.5]">
+      <div className="p-20px">
+        <Share />
+      </div>
+    </div>
+  );
 }
