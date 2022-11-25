265379ee340c25660ea77974df20b069280994a7
Mizan <mizan@Mizans-Mac-mini.local>
2022-11-25 20:52:21 +0600

    SB-api-dbsetup

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,17 +1,21 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
-import { MongooseModule } from '@nestjs/mongoose';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UserModule } from './user/user.module';
-import { AuthModule } from './auth/auth.module';
+
 import { config } from 'dotenv';
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { environment } from './environment/environment';
+import { User } from './user/entities/user.entity';
 config();
 
-const mongoDbUrl = process.env.MONGO_URL;
+const ormConfig = {
+  ...environment.database,
+  entities: [User],
+} as TypeOrmModuleOptions;
 
 @Module({
-  imports: [MongooseModule.forRoot(mongoDbUrl), UserModule, AuthModule],
+  imports: [TypeOrmModule.forRoot(ormConfig)],
   controllers: [AppController],
   providers: [AppService],
 })
